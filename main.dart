661e873b6498d7929a4abf82dbcb48b44685e372
ahmetcj4 <ahmetcj4@hotmail.com>
2018-08-12 15:25:50 +0300

    add error handling

--- main.dart
+++ main.dart
@@ -3,18 +3,35 @@ import 'dart:io';
 
 final File file = new File('index.html');
 Future main() async {
-  HttpServer server = await HttpServer.bind(
-    "127.0.0.1",
-    8080,
-  ); 
+  HttpServer server;
+  try {
+    server = await HttpServer.bind(
+      "127.0.0.1",
+      8080,
+    );
+  } catch (e) {
+    print('Failed to start server $e');
+    exit(-1);
+  }
+
   print("Listenning on Localhost:${server.port}");
 
-  await for (var req in server){
-    if (await file.exists()){
+  await for (var req in server) {
+    if (await file.exists()) {
       print('Serving ${file.path}');
       req.response.headers.contentType = ContentType.HTML;
 
-      await file.openRead().pipe(req.response);
+      try {
+        await file.openRead().pipe(req.response);
+      } catch (e) {
+        print('Couldn\'t read file $e');
+        exit(-1);
+      }
+    } else {
+      print('Can\'t open file');
+      req.response
+        ..statusCode = HttpStatus.NOT_FOUND
+        ..close();
     }
   }
 }
